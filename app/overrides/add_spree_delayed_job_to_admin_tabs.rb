b7fa18a74e8af07f1bd811470fecfe78a511ccef
Frank <frankiek3@gmail.com>
2015-08-14 21:53:22 -0400

    Update

--- app/overrides/add_spree_delayed_job_to_admin_tabs.rb
+++ app/overrides/add_spree_delayed_job_to_admin_tabs.rb
@@ -1,9 +1,8 @@
 Deface::Override.new(
-  :virtual_path => 'spree/layouts/admin',
+  :virtual_path => 'spree/admin/shared/_menu',
   :name => 'add_spree_delayed_job_to_admin_tabs',
   :insert_bottom => "[data-hook='admin_tabs']",
   :text => %q{
-    <%= tab 'Batch Jobs' ,:route => :admin_delayed_jobs %>
+    <%= tab :batch_jobs, :route => :admin_delayed_jobs %>
   })
-                        
 
--- lib/spree_delayed_job.rb
+++ lib/spree_delayed_job.rb
@@ -4,6 +4,7 @@ require "spree_delayed_job/delayed_rake"
 module SpreeDelayedJob
   class Engine < Rails::Engine
     railtie_name "spree_delayed_job"
+    engine_name 'spree_delayed_job'
 
     config.autoload_paths += %W(#{config.root}/lib)
 
